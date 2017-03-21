4721e0c44ee6fb90b39c1eb0f470ac3925dc7e2d
Ain <ain.salula@gmail.com>
2017-03-22 01:14:39 +0200

    using co package

--- 06.js
+++ 06.js
@@ -1,5 +1,6 @@
 const url = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
 const fetch = require('node-fetch');
+const co = require('co');
 
 function* createQuoteFetcher() {
   const response = yield fetch(url);
@@ -7,15 +8,5 @@ function* createQuoteFetcher() {
   return `${quote.quoteText} - ${quote.quoteAuthor}`;
 }
 
-const coroutine = (gen) => {
-  const generator = gen();
-  const handle = (result) => {
-    if (result.done) return Promise.resolve(result.value);
-    return Promise.resolve(result.value)
-      .then(res => handle(generator.next(res)));
-  }
-  return handle(generator.next());
-}
-
-const quoteFetcher = coroutine(createQuoteFetcher);
+const quoteFetcher = co(createQuoteFetcher);
 quoteFetcher.then(quote => console.log(quote));
